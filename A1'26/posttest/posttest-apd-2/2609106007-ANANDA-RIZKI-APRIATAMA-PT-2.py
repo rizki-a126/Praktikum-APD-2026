f9d13358c371c806aab35cd0dc26b0cbfb1bfea3
rizki-a126 <anandarizkiapriatama@gmail.com>
2026-09-19 20:55:23 +0700

    Add files via upload

--- A1'26/posttest/posttest-apd-2/2609106007-ANANDA-RIZKI-APRIATAMA-PT-2.py
+++ A1'26/posttest/posttest-apd-2/2609106007-ANANDA-RIZKI-APRIATAMA-PT-2.py
@@ -10,20 +10,30 @@ total_barang = [barang_1, barang_2, barang_3, barang_4, barang_5, barang_6]
 total_harga = barang_1 + barang_2 + barang_3 + barang_4 + barang_5 + barang_6
 total_pajak = total_harga * 0.15
 total_bayar = total_harga + total_pajak
-print("Total harga yang harus dibayar adalah Rp", total_bayar)
 
 rata_rata = total_bayar / len(total_barang)
-print("Rata-rata harga barang dengan pajak adalah Rp", rata_rata)
 
 nim = 7
-boolean = nim < rata_rata
-print("Apakah NIM lebih kecil dari rata-rata harga barang?", boolean)
+bolean = nim < rata_rata
 
 total_ringgit = total_bayar / 4360
-print("Total harga dalam Ringgit Malaysia adalah RM", total_ringgit)
 
 total_franc_swiss = total_bayar / 21665.05
-print("Total harga dalam Swiss Franc adalah CHF", total_franc_swiss)
 
 barang_ganjil = total_barang[0:5:2]
+
+print("Barang 1: Rp", barang_1)
+print("Barang 2: Rp", barang_2)
+print("Barang 3: Rp", barang_3)
+print("Barang 4: Rp", barang_4)
+print("Barang 5: Rp", barang_5)
+print("Barang 6: Rp", barang_6)
+print("Total harga barang sebelum pajak adalah Rp", total_harga)
+print("Total pajak yang harus dibayar adalah Rp", total_pajak)
+print("Total harga barang dengan pajak adalah Rp", total_bayar)
+print("Rata-rata harga barang dengan pajak adalah Rp", rata_rata)
+print("NIM saya adalah", nim)
+print("Apakah NIM saya lebih kecil dari rata-rata harga barang?", bolean)
+print("Total harga barang dalam Ringgit Malaysia adalah RM", total_ringgit)
+print("Total harga barang dalam Swiss Franc adalah CHF", total_franc_swiss)
 print("Harga barang ganjil adalah:", barang_ganjil)
